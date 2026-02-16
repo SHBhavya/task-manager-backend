0a2db9a6f28e0b6dbb0104be88b144e235a107d8
S H Bhavya <shbhavya14@gmail.com>
2026-02-16 16:49:36 +0530

    Add user deletion API with database integration

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def get_db():
     finally:
         db.close()
 
-
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -65,3 +64,15 @@ def update_user(
     db.refresh(db_user)
 
     return db_user
+
+@app.delete("/users/{user_id}")
+def delete_user(
+    user_id: int = Path(..., gt=0, lt=50),
+    db: Session = Depends(get_db)
+):
+    user = db.query(models.User).filter(models.User.user_id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    db.delete(user)
+    db.commit()
+    return user
