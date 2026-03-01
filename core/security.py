b7fd789b34bb9ded390f1f0de854fff569e0ed70
S H Bhavya <shbhavya14@gmail.com>
2026-03-01 22:42:14 +0530

    feat(auth): add refresh token support

--- core/security.py
+++ core/security.py
@@ -29,6 +29,11 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=7)  # long expiry
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def get_current_user(
     token: str = Depends(oauth2_scheme),
--- routers/auth.py
+++ routers/auth.py
@@ -1,19 +1,52 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Body
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from database import get_db
+from jose import JWTError, jwt
 import models, schemas
+
 from core.security import (
+    SECRET_KEY,
+    ALGORITHM,
     hash_password,
     verify_password,
-    create_access_token
+    create_access_token,
+    create_refresh_token
 )
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
 )
+from pydantic import BaseModel
 
+class RefreshRequest(BaseModel):
+    refresh_token: str
+
+@router.post("/refresh")
+def refresh_token(request: RefreshRequest):
+    try:
+        payload = jwt.decode(
+            request.refresh_token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        if payload.get("type") != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid token type")
+
+        user_id = payload.get("sub")
+
+        new_access_token = create_access_token({"sub": user_id})
+
+        return {
+            "access_token": new_access_token,
+            "token_type": "bearer"
+        }
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+    
 @router.post("/register", response_model=schemas.UserResponse)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
@@ -49,11 +82,12 @@ def login(
     if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    access_token = create_access_token(
-        data={"sub": user.email}
-    )
+    access_token = create_access_token({"sub": str(user.user_id)})
+    refresh_token = create_refresh_token({"sub": str(user.user_id)})
+
 
     return {
         "access_token": access_token,
+        "refresh_token": refresh_token,
         "token_type": "bearer"
     }
